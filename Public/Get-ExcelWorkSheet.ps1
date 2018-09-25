ab68e768fc4472c38e5c97263759fc0737286ae9
PrzemyslawKlys <pklys@evotec.pl>
2018-09-25 20:24:43 +0200

    Improvements to Get-ExcelWorkSheet

--- Public/Get-ExcelWorkSheet.ps1
+++ Public/Get-ExcelWorkSheet.ps1
@@ -4,16 +4,27 @@ function Get-ExcelWorkSheet {
     param (
         [OfficeOpenXml.ExcelPackage]  $ExcelDocument,
         [string] $Name,
+        [int] $Index,
         [switch] $All
     )
     if ($ExcelDocument) {
+        if ($Name -and $Index) {
+            Write-Warning 'Get-ExcelWorkSheet - Only $Name or $Index of Worksheet can be used.'
+            return
+        }
         if ($All) {
             $Data = $ExcelDocument.Workbook.Worksheets
-        } else {
-            $Data = $ExcelDocument.Workbook.Worksheets | Where { $_.Name -eq $Name }
+        } elseif ($Name -or $Index) {
+            if ($Name) {
+                $Data = $ExcelDocument.Workbook.Worksheets | Where { $_.Name -eq $Name }
+            }
+            if ($Index) {
+                if ($PSEdition -ne 'Core') {
+                    $Index = $Index + 1
+                }
+                $Data = $ExcelDocument.Workbook.Worksheets[$Index]
+            }
         }
-        return $Data
-    } else {
-        return
     }
+    return $Data
 }
