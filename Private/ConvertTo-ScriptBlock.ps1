359e332cf220ad477a8456d120112d37e0b2e3a0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-21 09:54:38 +0100

    Update

---
+++ Private/ConvertTo-ScriptBlock.ps1
@@ -0,0 +1,32 @@
+﻿function ConvertTo-ScriptBlock {
+    [cmdletBinding()]
+    param(
+        [Array] $Code,
+        [string[]] $Include,
+        [string[]] $Exclude
+    )
+    if ($Include) {
+        $Output = foreach ($Line in $Code) {
+            foreach ($I in $Include) {
+                if ($Line.StartsWith($I)) {
+                    $Line
+                }
+            }
+        }
+    }
+    if ($Exclude) {
+        $Output = foreach ($Line in $Code) {
+            $Tests = foreach ($E in $Exclude) {
+                if ($Line.StartsWith($E)) {
+                    $true
+                }
+            }
+            if ($Tests -notcontains $true) {
+                $Line
+            }
+        }
+    }
+    if ($Output) {
+        [ScriptBlock]::Create($Output)
+    }
+}
